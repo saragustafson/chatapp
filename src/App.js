9e11471bd0d9baa13e8cdef5c8a47cd6773d0b18
saragustafson <saramgustafson@gmail.com>
2020-07-07 12:21:16 -0700

    useDatu

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import React, {useState} from 'react';
 import './App.css';
 import Footer from './footer'
 import Message from './message'
+import {initialize, useDatu} from 'datu'
 
 function App() {
-  const[messages,setMessages] = useState([])
+  const{messages, send} = useDatu()
+  console.log(messages)
   return (
     <main className="main">
 
@@ -15,10 +17,12 @@ function App() {
           href="https://fonts.googleapis.com/css?family=Spartan"/>
     </header>
 
-    {messages.map((m,i)=> <Message key={i} text={m} />)}
+    <div className="messages">
+      {messages.map((m,i)=> <Message key={i} text={m.text} />)}
+    </div>
 
     <Footer 
-      onSend={(text)=> setMessages([...messages, text])}
+      onSend={(text)=> send({text:text})}
     />
 
     </main>
@@ -26,3 +30,16 @@ function App() {
 }
 
 export default App;
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDWXVgUqm3xATyzqUqTxcpvsW7U804ctXI",
+  authDomain: "chatter20202020.firebaseapp.com",
+  databaseURL: "https://chatter20202020.firebaseio.com",
+  projectId: "chatter20202020",
+  storageBucket: "chatter20202020.appspot.com",
+  messagingSenderId: "630230183323",
+  appId: "1:630230183323:web:cc967f51fc79e394ca053e"
+};
+initialize(firebaseConfig)
+
